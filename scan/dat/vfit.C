8845492263d31c4631da08d44514a508fe96b33a
aghaei <aghaei@mppmu.mpg.de>
2009-05-29 15:07:01 +0000

    fitted the data, hossein
    git-svn-id: file:///home/jingliu/home/jing.liu/svn/gerdalinchenII@555 a066fb6a-a811-11dc-9a5e-f027034fa78f

--- scan/dat/vfit.C
+++ scan/dat/vfit.C
@@ -25,18 +25,18 @@ void vfit(Int_t theAngle=0)
    A0Tree->AddFile(Form("/remote/pclg-10/aghaei/scan/dat/super/phi%d.root",theAngle));
 
    cc1->cd(1);
-   A0Tree->Draw("Cha_MCAEnergy[0]>>hdat0(200,2500.0,3500.0)");
+   A0Tree->Draw("Cha_MCAEnergy[0]>>hdat0(200,2300.0,4000.0)");
    TF1 *FitFcn0 = new TF1("FitFcn0","[0]*gausn(1)+pol2(4)");
    FitFcn0->SetParNames("bin width","events","mean","sigma","pol0","pol1","pol2");
 
    // fit data
-   FitFcn0->SetParameters(1.0,5000.0,3050.0,10.0,0,0,0);
+   FitFcn0->SetParameters(1.0,5000.0,3050.0,5.0,0,0,0);
    FitFcn0->FixParameter(0,hdat0->GetBinWidth(1));
    
    hdat0->Fit("FitFcn0");
 
-   Double_t N_core = FitFcn0->GetParameter("events");
-   Double_t dN_core= FitFcn0->GetParError(1);
+   Int_t N_core = FitFcn0->GetParameter("events");
+   Int_t dN_core= FitFcn0->GetParError(1);
 
 //=============================================================
 
@@ -44,66 +44,68 @@ void vfit(Int_t theAngle=0)
    A13Tree->AddFile(Form("/remote/pclg-10/aghaei/scan/dat/super/phi%d.root",theAngle));
 
    cc1->cd(2);
-   A13Tree->Draw("Cha_MCAEnergy[13]>>hdat13(200,3000.0,4500.0)");
+   A13Tree->Draw("Cha_MCAEnergy[13]>>hdat13(200,2900.0,5100.0)");
    TF1 *FitFcn13 = new TF1("FitFcn13","[0]*gausn(1)+pol2(4)");
    FitFcn13->SetParNames("bin width","events","mean","sigma","pol0","pol1","pol2");
 
    // fit data
-   FitFcn13->SetParameters(1.0,5000.0,3050.0,10.0,0,0,0);
+   FitFcn13->SetParameters(1.0,5000.0,3450.0,6.0,0,0,0);
    FitFcn13->FixParameter(0,hdat13->GetBinWidth(1));
    
    hdat13->Fit("FitFcn13");
 
-   Double_t N_13 = FitFcn13->GetParameter("events");
-   Double_t dN_13= FitFcn13->GetParError(1);
+   Int_t N_13 = FitFcn13->GetParameter("events");
+   Int_t dN_13= FitFcn13->GetParError(1);
 
 //=============================================================
    TChain *A14Tree = new TChain("PSTree");
    A14Tree->AddFile(Form("/remote/pclg-10/aghaei/scan/dat/super/phi%d.root",theAngle));
 
    cc1->cd(3);
-   A14Tree->Draw("Cha_MCAEnergy[14]>>hdat14(200,3000.0,4500.0)");
+   A14Tree->Draw("Cha_MCAEnergy[14]>>hdat14(200,2900.0,5100.0)");
    TF1 *FitFcn14 = new TF1("FitFcn14","[0]*gausn(1)+pol2(4)");
    FitFcn14->SetParNames("bin width","events","mean","sigma","pol0","pol1","pol2");
 
    // fit data
-   FitFcn14->SetParameters(1.0,5000.0,3050.0,10.0,0,0,0);
+   FitFcn14->SetParameters(1.0,5000.0,3700.0,5.0,0,0,0);
    FitFcn14->FixParameter(0,hdat14->GetBinWidth(1));
    
    hdat14->Fit("FitFcn14");
 
-   Double_t N_14 = FitFcn14->GetParameter("events");
-   Double_t dN_14= FitFcn14->GetParError(1);
+   Int_t N_14 = FitFcn14->GetParameter("events");
+   Int_t dN_14= FitFcn14->GetParError(1);
 //=============================================================
 
    TChain *A15Tree = new TChain("PSTree");
    A15Tree->AddFile(Form("/remote/pclg-10/aghaei/scan/dat/super/phi%d.root",theAngle));
 
    cc1->cd(4);
-   A15Tree->Draw("Cha_MCAEnergy[15]>>hdat15(200,3000.0,4500.0)");
+   A15Tree->Draw("Cha_MCAEnergy[15]>>hdat15(200,2900.0,5100.0)");
    TF1 *FitFcn15 = new TF1("FitFcn15","[0]*gausn(1)+pol2(4)");
    FitFcn15->SetParNames("bin width","events","mean","sigma","pol0","pol1","pol2");
 
    // fit data
-   FitFcn15->SetParameters(1.0,5000.0,3050.0,10.0,0,0,0);
+   FitFcn15->SetParameters(1.0,5000.0,3700.0,25.0,0,0,0);
    FitFcn15->FixParameter(0,hdat15->GetBinWidth(1));
    
    hdat15->Fit("FitFcn15");
 
-   Double_t N_15 = FitFcn15->GetParameter("events");
-   Double_t dN_15 = FitFcn15->GetParError(1);
+   Int_t N_15 = FitFcn15->GetParameter("events");
+   Int_t dN_15 = FitFcn15->GetParError(1);
    
 //=============================================================
-   cout<<" total number of events in the core "
+   cout<<""<<theAngle<<"    &"<<N_core<<" $\\pm$ "<<dN_core<<"    &"<<N_13<<" $\\pm$ "<<dN_13<<"    &"<<N_14<<" $\\pm$ "<<dN_14<<"    &"<<N_15<<" $\\pm$ "<<dN_15<<"    &\\\\ \\hline"<<endl;
+
+   cout<<" total number of events at "<<theAngle<<" degrees in the core "
        <<N_core<<" plus-minus "<<dN_core<<endl;
 
-   cout<<" total number of events in seg  13 "
+   cout<<" total number of events at "<<theAngle<<" degrees in seg  13 "
        <<N_13<<" plus-minus "<<dN_13<<endl;
 
-   cout<<" total number of events in seg  14 "
+   cout<<" total number of events at "<<theAngle<<" degrees in seg  14 "
        <<N_14<<" plus-minus "<<dN_14<<endl;
 
-    cout<<" total number of events in seg  15 "
+    cout<<" total number of events at "<<theAngle<<" degrees in seg  15 "
 	<<N_15<<" plus-minus "<<dN_15<<endl;
 
 }
